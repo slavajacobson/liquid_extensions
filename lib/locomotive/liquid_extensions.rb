c5902f473f2b7b8fab6d42188dd8bddde06d2587
Slava Y <vs.jacobson@gmail.com>
2015-08-04 00:48:42 -0400

    OK

--- lib/locomotive/liquid_extensions.rb
+++ lib/locomotive/liquid_extensions.rb
@@ -5,11 +5,11 @@ require 'net/http'
 %w{misc tags filters}.each do |dir|
   Dir[File.join(File.dirname(__FILE__), 'liquid_extensions', dir, '*.rb')].each { |lib| require lib }
 end
-
+Liquid::Template.register_filter(Locomotive::LiquidExtensions::Filters::Dimensions)
 Liquid::Template.register_filter(Locomotive::LiquidExtensions::Filters::Number)
 Liquid::Template.register_filter(Locomotive::LiquidExtensions::Filters::Math)
 Liquid::Template.register_filter(Locomotive::LiquidExtensions::Filters::Sample)
 Liquid::Template.register_filter(Locomotive::LiquidExtensions::Filters::Json)
 Liquid::Template.register_filter(Locomotive::LiquidExtensions::Filters::Hexdigest)
 # DEBUG
-# puts "[LocomotiveLiquidExtensions] tags and filters loaded"
+puts "[LocomotiveLiquidExtensions] tags and filters loaded"
---
+++ lib/locomotive/liquid_extensions/filters/dimensions.rb
@@ -0,0 +1,14 @@
+module Locomotive
+  module LiquidExtensions
+    module Filters
+      module Dimensions
+
+        def dimensions(input, *options)
+          byebug
+          t = "1"
+        end
+
+      end
+    end
+  end
+end
